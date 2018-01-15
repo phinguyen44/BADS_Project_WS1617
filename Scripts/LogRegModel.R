8345fbd176280d3c29c9061a0e7c1def5bf60185
Claudia <claudia-jena@gmx.de>
2018-01-15 11:43:10 +0100

    Update Log Reg

---
+++ Scripts/LogRegModel.R
@@ -0,0 +1,11 @@
+
+################################################################################
+# 
+#   Logistic Regression
+#
+################################################################################
+# Description:
+#  - creates log regression baseline model
+#  - N-fold crossvaliadation
+#  - variable selection via mix of filtering & wrappers
+################################################################################
