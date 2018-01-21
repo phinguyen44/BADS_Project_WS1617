692c44959860de3bbcdabcccb24d7e18c4d400f7
phister <nguyen1.618@gmail.com>
2018-01-21 19:26:26 +0100

    platt scaling added

--- Scripts/Helpful-Models.R
+++ Scripts/Helpful-Models.R
@@ -4,43 +4,113 @@
 ################################################################################
 # Description:
 # BADS project - includes 5 different model implementations using the mlr
-# package. CV is excluded and done separately.
+# package. inner loop CV also included for hyperparameter tuning
 # 
+# calib.part() - create data partitions for calibration
+# calib.mod() - run prediction through a calibrator
+#
 # logistic regression (glm, no regularization)
 # decision trees (rpart)
 # random forest (randomForest)
 # gradient boosting (xgb)
 # neural net (nnet) - single hidden layer neural network
+#
 # 
 ################################################################################
 
+# TODO: FOR EACH MODEL:
+# 1) add a feature selection wrapper (either filter or wrapper or both)
+
+# TODO: finish calibration for rest of models!
+
+calib.part <- function(tr) {
+    tr.p <- tr
+    
+    #  SPLIT DATA FOR PLATT SCALING
+    idx.p <- createDataPartition(y = tr.p$return, p = 0.8, list = FALSE)
+    
+    tr <- tr.p[idx.p, ]  # training set with platt
+    cs <- tr.p[-idx.p, ] # calib set
+    
+    tr.label <- tr$return
+    cs.label <- cs$return
+    
+    return(list(tr = tr, cs = cs, tr.label = tr.label, cs.label = cs.label))
+}
+
+calib.mod <- function(lr, calibtask) {
+    
+    # predict on calib set
+    calib.pred <- predict(lr, calibtask)
+    
+    # train logreg on calib set
+    clearner <- makeLearner("classif.logreg", predict.type = "prob")
+    calib.df <- data.frame(y = cs.label, x = calib.pred$data$prob.1)
+    ctask    <- makeClassifTask(data = calib.df, target = "y", positive = 1)
+    calib.m  <- train(clearner, ctask)
+    
+    # pass test set prediction through calibrated model
+    test.pred <- data.frame(y = lr.pred$data$truth, x = lr.pred$data$prob.1)
+    task <- makeClassifTask(data = test.pred, target = "y", positive = 1)
+    lr.pred.calib <- predict(calib.m, task)
+    
+    return(lr.pred.calib)
+}
+
 # LOGISTIC
-lr.mod <- function(learner, traintask, testtask) {
+lr.mod <- function(learner, tr, ts, calib = FALSE) {
     
     # make learner
     lr.model  <- makeLearner(learner, predict.type = "prob")
     
+    # split data for calibration
+    if (calib == TRUE) {
+        calib.data <- calib.part(tr)
+        tr         <- calib.data$tr
+        cs         <- calib.data$cs
+        cs.label   <- calib.data$cs.label
+        
+        calibtask <- makeClassifTask(data = cs, target = "return", positive = 1)
+    }
+    
+    traintask <- makeClassifTask(data = tr, target = "return", positive = 1)
+    testtask  <- makeClassifTask(data = ts, target = "return", positive = 1)
+    
     start <- Sys.time()
     # train model
     lr      <- train(lr.model, traintask)
-    
     # predict
     lr.pred <- predict(lr, testtask)
-    model   <- lr$learner.model
+
+    # pass prediction through calibrated model
+    if (calib == TRUE) lr.pred.calib <- calib.mod(lr, calibtask)
     
     end     <- Sys.time()
     runtime <- end - start
     cat(paste0(learner, " run time: "))
     print(runtime)
     
-    return(list(pred = lr.pred, model = model))
+    return(list(pred = lr.pred, pred.calib = lr.pred.calib))
 }
 
 # DECISION TREE
-dt.mod <- function(learner, traintask, testtask) {
+dt.mod <- function(learner, tr, ts, calib = FALSE) {
     
     # make learner
     makeatree <- makeLearner(learner, predict.type = "prob")
+    
+    # split data for calibration
+    if (calib == TRUE) {
+        calib.data <- calib.part(tr)
+        tr         <- calib.data$tr
+        cs         <- calib.data$cs
+        cs.label   <- calib.data$cs.label
+        
+        calibtask <- makeClassifTask(data = cs, target = "return", positive = 1)
+    }
+    
+    traintask <- makeClassifTask(data = tr, target = "return", positive = 1)
+    testtask  <- makeClassifTask(data = ts, target = "return", positive = 1)
     
     # cross-validation
     set_cv <- makeResampleDesc("CV",iters = 4L)
@@ -78,21 +148,37 @@ dt.mod <- function(learner, traintask, testtask) {
     # predict
     t.pred  <- predict(t.rpart, testtask)
     
+    # pass prediction through calibrated model
+    if (calib == TRUE) t.pred.calib <- calib.mod(t.rpart, calibtask)
+    
     end     <- Sys.time()
     runtime <- end - start
     cat(paste0(learner, " run time: "))
     print(runtime)
     
-    return(list(pred = t.pred, pars = hyperpars))
+    return(list(pred = t.pred, pred.calib = t.pred.calib, pars = hyperpars))
     
 }
 
 # RANDOM FOREST
-rf.mod <- function(learner, traintask, testtask) {
+rf.mod <- function(learner, tr, ts, calib = FALSE) {
     
     # make Learner
     rf <- makeLearner(learner, predict.type = "prob", 
                       par.vals = list(ntree = 200, mtry = 3, importance = TRUE))
+    
+    # split data for calibration
+    if (calib == TRUE) {
+        calib.data <- calib.part(tr)
+        tr         <- calib.data$tr
+        cs         <- calib.data$cs
+        cs.label   <- calib.data$cs.label
+        
+        calibtask <- makeClassifTask(data = cs, target = "return", positive = 1)
+    }
+    
+    traintask <- makeClassifTask(data = tr, target = "return", positive = 1)
+    testtask  <- makeClassifTask(data = ts, target = "return", positive = 1)
     
     # hyperparameters
     rf_param <- makeParamSet(
@@ -126,23 +212,40 @@ rf.mod <- function(learner, traintask, testtask) {
     # predict
     rf.pred  <- predict(rforest, testtask)
     
+    # pass prediction through calibrated model
+    if (calib == TRUE) rf.pred.calib <- calib.mod(rforest, calibtask)
+    
     end     <- Sys.time()
     runtime <- end - start
     cat(paste0(learner, " run time: "))
     print(runtime)
-
-    return(list(pred = rf.pred, pars = hyperpars))
+    
+    return(list(pred = rf.pred, pred.calib = rf.pred.calib, pars = hyperpars))
 }
 
 # XGB
-xgb.mod <- function(learner, traintask, testtask) {
+xgb.mod <- function(learner, tr, ts, calib = FALSE) {
+    
+    # make learner
+    xg_set <- makeLearner(learner, predict.type = "prob")
+    
+    # split data for calibration
+    if (calib == TRUE) {
+        calib.data <- calib.part(tr)
+        tr         <- calib.data$tr
+        cs         <- calib.data$cs
+        cs.label   <- calib.data$cs.label
+        
+        calibtask <- makeClassifTask(data = cs, target = "return", positive = 1)
+        calibtask <- createDummyFeatures(obj = calibtask)
+    }
+    
+    traintask <- makeClassifTask(data = tr, target = "return", positive = 1)
+    testtask  <- makeClassifTask(data = ts, target = "return", positive = 1)
     
     # one hot encoding
     traintask <- createDummyFeatures(obj = traintask) 
     testtask  <- createDummyFeatures(obj = testtask)
-    
-    # make learner
-    xg_set <- makeLearner(learner, predict.type = "prob")
     
     rancontrol <- makeTuneControlRandom(maxit = 30L)
     set_cv     <- makeResampleDesc("CV",iters = 4L)
@@ -176,19 +279,34 @@ xgb.mod <- function(learner, traintask, testtask) {
     # predict
     xg_pred  <- predict(xg_model, testtask)
     
+    # pass prediction through calibrated model
+    if (calib == TRUE) xg.pred.calib <- calib.mod(xg_model, calibtask)
+    
     end     <- Sys.time()
     runtime <- end - start
     cat(paste0(learner, " run time: "))
     print(runtime)
     
-    return(list(pred = xg_pred, pars = hyperpars))
-    
+    return(list(pred = xg_pred, pred.calib = xg.pred.calib, pars = hyperpars))
 }
 
 # NNET
-nn.mod <- function(learner, traintask, testtask) {
+nn.mod <- function(learner, tr, ts, calib = FALSE) {
     
     nn <- makeLearner(learner, predict.type = "prob")
+    
+    # split data for calibration
+    if (calib == TRUE) {
+        calib.data <- calib.part(tr)
+        tr         <- calib.data$tr
+        cs         <- calib.data$cs
+        cs.label   <- calib.data$cs.label
+        
+        calibtask <- makeClassifTask(data = cs, target = "return", positive = 1)
+    }
+    
+    traintask <- makeClassifTask(data = tr, target = "return", positive = 1)
+    testtask  <- makeClassifTask(data = ts, target = "return", positive = 1)
     
     rancontrol <- makeTuneControlRandom(maxit = 30L)
     set_cv     <- makeResampleDesc("CV",iters = 4L)
@@ -218,11 +336,14 @@ nn.mod <- function(learner, traintask, testtask) {
     # predict
     nn_pred <- predict(nn_mod, testtask)
     
+    # pass prediction through calibrated model
+    if (calib == TRUE) nn.pred.calib <- calib.mod(nn_mod, calibtask)
+    
     end     <- Sys.time()
     runtime <- end - start
     cat(paste0(learner, " run time: "))
     print(runtime)
     
-    return(list(pred = nn_pred, pars = hyperpars))
+    return(list(pred = nn_pred, pred.calib = nn.pred.calib, pars = hyperpars))
     
 }
