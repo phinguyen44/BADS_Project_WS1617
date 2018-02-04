02e69aa7a82ad9398c912d1a8259ed59b737459d
phister <nguyen1.618@gmail.com>
2018-02-04 10:56:20 +0100

    retuning parameters and removing decision tree'

--- Scripts/Helpful-Models.R
+++ Scripts/Helpful-Models.R
@@ -10,7 +10,6 @@
 # calib.mod() - run prediction through a calibrator
 #
 # logistic regression (glm, no regularization)
-# decision trees (rpart)
 # random forest (randomForest)
 # gradient boosting (xgb)
 # neural net (nnet) - single hidden layer neural network
@@ -101,75 +100,6 @@ lr.mod <- function(learner, tr, ts, calib = FALSE) {
     return(output)
 }
 
-# DECISION TREE
-dt.mod <- function(learner, tr, ts, calib = FALSE) {
-
-    # split data for calibration
-    if (calib == TRUE) {
-        calib.data <- calib.part(tr)
-        tr         <- calib.data$tr
-        cs         <- calib.data$cs
-        cs.label   <- calib.data$cs.label
-    }
-
-    traintask <- makeClassifTask(data = tr, target = "return", positive = 1)
-
-    # make learner
-    makeatree <- makeLearner(learner, predict.type = "prob")
-
-    # cross-validation
-    set_cv <- makeResampleDesc("CV",iters = 4L)
-
-    # hyperparameters
-    gs <- makeParamSet(
-        makeIntegerParam("minsplit",lower = 10, upper = 30),
-        makeIntegerParam("minbucket", lower = 5, upper = 30),
-        makeNumericParam("cp", lower = 0.001, upper = 0.05)
-    )
-
-    # grid search
-    # gscontrol <- makeTuneControlGrid()
-    # random search
-    gscontrol <- makeTuneControlRandom(maxit = 20L)
-
-    start <- Sys.time()
-
-    parallelStartSocket(cpus = detectCores())
-    stune <- tuneParams(learner    = makeatree,
-                        resampling = set_cv,
-                        task       = traintask,
-                        par.set    = gs,
-                        control    = gscontrol,
-                        measures   = mlr::auc)
-    parallelStop()
-
-    # use hyperparameters
-    t.tree    <- setHyperPars(makeatree, par.vals = stune$x)
-    hyperpars <- stune$x
-
-    # train model
-    t.rpart <- mlr::train(t.tree, traintask)
-
-    # predict
-    t.pred  <- predict(t.rpart, newdata = ts)
-
-    # pass prediction through calibrated model
-    if (calib == TRUE) {
-        t.pred.calib <- calib.mod(t.rpart, t.pred, cs, cs.label)
-    }
-
-    end     <- Sys.time()
-    runtime <- end - start
-    cat(paste0(learner, " run time: "))
-    print(runtime)
-
-    output <- list(pred = t.pred, pars = hyperpars)
-    if (calib == TRUE) output[['pred.calib']] <- t.pred.calib
-
-    return(output)
-
-}
-
 # RANDOM FOREST
 rf.mod <- function(learner, tr, ts, calib = FALSE) {
 
@@ -185,18 +115,18 @@ rf.mod <- function(learner, tr, ts, calib = FALSE) {
 
     # make Learner
     rf <- makeLearner(learner, predict.type = "prob",
-                      par.vals = list(ntree = 200, mtry = 3, importance = TRUE))
+                      par.vals = list(ntree = 500, mtry = 3, importance = TRUE))
 
     # hyperparameters
     rf_param <- makeParamSet(
-        makeDiscreteParam("ntree", values = seq(200, 800, by=100)),
-        makeDiscreteParam("nodesize", values = seq(1, 50, by = 4)),
-        makeIntegerParam("mtry", lower = 1, upper = 8)
+        makeDiscreteParam("ntree", seq(300, 500, by = 50)),
+        makeIntegerParam("mtry", lower = 3, upper = 10),
+        makeDiscreteParam("nodesize", seq(10, 50, by = 5))
     )
 
     # tune parameters (random rather than grid search faster)
-    rancontrol <- makeTuneControlRandom(maxit = 30L)
-    set_cv     <- makeResampleDesc("CV",iters = 4L)
+    rancontrol <- makeTuneControlRandom(maxit = 20L)
+    set_cv     <- makeResampleDesc("CV", iters = 3L)
 
     start <- Sys.time()
 
@@ -255,16 +185,16 @@ xgb.mod <- function(learner, tr, ts, calib = FALSE) {
     # make learner
     xg_set <- makeLearner(learner, predict.type = "prob")
 
-    rancontrol <- makeTuneControlRandom(maxit = 30L)
-    set_cv     <- makeResampleDesc("CV",iters = 4L)
+    rancontrol <- makeTuneControlRandom(maxit = 20L)
+    set_cv     <- makeResampleDesc("CV", iters = 3L)
 
     xg_ps <- makeParamSet(
-        makeDiscreteParam("booster", values = c("gbtree")),
-        makeDiscreteParam("gamma", values = c(0, 0.1, 1, 10)),
+        makeDiscreteParam("booster", values = c("gbtree", "dart")),
+        makeDiscreteParam("gamma", values = c(0, 0.1, 0.2)),
         makeDiscreteParam("eta", values = c(0.001, 0.01, 0.1, 0.3, 0.8)),
-        makeDiscreteParam("nrounds", values = c(1, 50, 100, 200, 400)),
-        makeDiscreteParam("lambda", values = seq(0, 1, by=0.1)),
-        makeIntegerParam("max_depth", lower = 2L, upper = 15L)
+        makeDiscreteParam("nrounds", values = c(50, 100, 200)),
+        makeDiscreteParam("lambda", values = seq(0.3, 0.6, by=0.05)),
+        makeIntegerParam("max_depth", lower = 3L, upper = 8L)
     )
 
     start <- Sys.time()
@@ -318,8 +248,8 @@ nn.mod <- function(learner, tr, ts, calib = FALSE) {
 
     nn <- makeLearner(learner, predict.type = "prob")
 
-    rancontrol <- makeTuneControlRandom(maxit = 30L)
-    set_cv     <- makeResampleDesc("CV",iters = 4L)
+    rancontrol <- makeTuneControlRandom(maxit = 20L)
+    set_cv     <- makeResampleDesc("CV",iters = 3L)
 
     nn_par <- makeParamSet(
         makeDiscreteParam("size", values = seq(3, 8, by=1)),
